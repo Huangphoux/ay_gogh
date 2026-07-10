397421a084ded6cb0fbff253abfb7a8dfc2fd826
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2026-07-10 08:42:35 +0700

    feat: <details> due words and retired words

--- read.py
+++ read.py
@@ -488,30 +488,34 @@ def chapter_main(auth, num: int, word: str = "", bypass: int = 0, context: str =
 
     before_complete = (
         Section(
-            H2("Due words"),
-            Ul(
-                *(
-                    Li(
-                        style="user-select: none; cursor: pointer;",
-                        data_on_click=show_popup(num, True),
-                    )(c["front"])
-                    for c in due_cards
-                )
-            ),
+            Details(
+                Summary("Due words"),
+                Ul(
+                    *(
+                        Li(
+                            style="user-select: none; cursor: pointer;",
+                            data_on_click=show_popup(num, True),
+                        )(c["front"])
+                        for c in due_cards
+                    )
+                ),
+            )
         )
         if due_cards
         else None,
         Section(
-            H2("Retired words"),
-            Ul(
-                *(
-                    Li(
-                        style="user-select: none; cursor: pointer;",
-                        data_on_click=show_popup(num, True),
-                    )(c["front"])
-                    for c in retired_cards
-                )
-            ),
+            Details(
+                Summary("Retired words"),
+                Ul(
+                    *(
+                        Li(
+                            style="user-select: none; cursor: pointer;",
+                            data_on_click=show_popup(num, True),
+                        )(c["front"])
+                        for c in retired_cards
+                    )
+                ),
+            )
         )
         if retired_cards
         else None,
