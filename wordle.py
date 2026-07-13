e3302074a9a6c474edef512d26c1dac09dccaadf
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2026-07-13 21:42:22 +0700

    fix: WIP Refactoring Wordle
    Convert game state into a function

--- wordle.py
+++ wordle.py
@@ -17,11 +17,7 @@ def wordle(auth):
 @sse
 async def cqrs(req, auth):
     async for _, data in relay.subscribe(f"wordle.{auth}"):
-        yield elements(
-            wordle_main(auth, data["game_state"] if "game_state" in data else None),
-            selector="main",
-            use_view_transition=True,
-        )
+        yield elements(wordle_main(auth), selector="main", use_view_transition=True)
 
 
 def set_new_word(auth):
@@ -79,7 +75,7 @@ def color_row(guess: str, target: str, is_submitted: int = 1) -> list[FT]:
     return row
 
 
-def wordle_main(auth, game_state: bool | None = None):
+def wordle_main(auth):
     guesses = list(db.get(auth).query("SELECT number, guess, is_submitted FROM wordle"))
 
     if guesses:
@@ -90,10 +86,10 @@ def wordle_main(auth, game_state: bool | None = None):
             db.get(auth).query("SELECT number, guess, is_submitted FROM wordle")
         )
 
-    h1 = H1(
-        id="main-heading",
-        style=f"view-transition-name: wordle",
-    )(f"NGSL Wordle")
+    h1 = H1(id="main-heading", style=f"view-transition-name: wordle")(f"NGSL Wordle")
+
+    game_state = get_game_state(auth)
+    print(game_state)
 
     new_word = (
         Button(data_on_click=delete("/wordle/new"))("New Word")
@@ -158,26 +154,58 @@ def wordle_main(auth, game_state: bool | None = None):
     )
 
 
-@rt.delete("/new")
-def new(auth):
-    db.get(auth).execute(
-        "DELETE FROM wordle",
+def get_game_state(auth) -> bool | None:
+    """True=win, False=lose, None=in progress"""
+
+    last: dict[str, str] = get_last(auth, is_submitted=True)
+    target: str = db.get(auth).item("SELECT guess FROM wordle WHERE number=0")
+
+    if last["guess"] == target:
+        return True
+    elif last["number"] is not None and int(last["number"]) == 6:
+        return False
+    else:
+        return None
+
+
+def get_last(auth, is_submitted: bool = False) -> dict[str, str]:
+    where: str = (
+        "is_submitted=0" if not is_submitted else "is_submitted=1 AND number<>0"
     )
 
+    return list(
+        db.get(auth).query(
+            f"""
+            SELECT MIN(number) AS number, guess
+            FROM wordle
+            WHERE {where}"""
+        )
+    )[0]
+
+
+def get_target(auth) -> dict[str, str]:
+    return list(
+        db.get(auth).query(
+            "SELECT guess, LENGTH(guess) AS length FROM wordle WHERE number=0"
+        )
+    )[0]
+
+
+def get_length(auth) -> int:
+    return int(len(db.get(auth).item("SELECT guess FROM wordle WHERE number=0")))
+
+
+@rt.delete("/new")
+def new(auth):
+    db.get(auth).execute("DELETE FROM wordle")
     relay.publish(f"wordle.{auth}", {})
 
 
 @rt.put("/type/{key:str}")
 def type(auth, key: str):
-    last: dict = list(
-        db.get(auth).query(
-            "SELECT MIN(number) AS number, guess FROM wordle WHERE is_submitted=0"
-        )
-    )[0]
+    last: dict[str, str] = get_last(auth)
 
-    length: int = int(len(db.get(auth).item("SELECT guess FROM wordle WHERE number=0")))
-
-    if len(last["guess"]) < length:
+    if len(last["guess"]) < get_length(auth):
         db.get(auth).execute(
             "UPDATE wordle SET guess=CONCAT(guess, ?) WHERE number=?",
             (key.upper(), last["number"]),
@@ -188,41 +216,20 @@ def type(auth, key: str):
 
 @rt.put("/enter")
 def enter(auth):
-    last: dict = list(
-        db.get(auth).query(
-            "SELECT MIN(number) AS number, guess FROM wordle WHERE is_submitted=0"
-        )
-    )[0]
+    last: dict[str, str] = get_last(auth)
 
-    target: dict = list(
-        db.get(auth).query(
-            "SELECT guess, LENGTH(guess) AS length FROM wordle WHERE number=0"
-        )
-    )[0]
-
-    if len(last["guess"]) == target["length"]:
+    if len(last["guess"]) == get_length(auth):
         db.get(auth).execute(
             "UPDATE wordle SET is_submitted=1 WHERE number=?",
             (last["number"],),
         )
 
-    game_state: bool | None = None  # True=win, False=lose, None=in progress
-
-    if last["guess"] == target["guess"]:
-        game_state = True
-    elif int(last["number"]) == 6:
-        game_state = False
-
-    relay.publish(f"wordle.{auth}", {"game_state": game_state})
+    relay.publish(f"wordle.{auth}", {})
 
 
 @rt.put("/backspace")
 def backspace(auth):
-    last: dict = list(
-        db.get(auth).query(
-            "SELECT MIN(number) AS number, guess FROM wordle WHERE is_submitted=0"
-        )
-    )[0]
+    last: dict[str, str] = get_last(auth)
 
     if len(last["guess"]) > 0:
         db.get(auth).execute(
